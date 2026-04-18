6bdf2d2bb465fcb3bc3ed4e4ba708bd05ea94ba2
makalestainton-creator <makalestainton@gmail.com>
2026-04-18 20:43:01 +0300

    fixed the folder structure and configured favicon handling for webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 export default {
   entry: "./src/homepage.js",
@@ -13,6 +14,18 @@ export default {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/favicon/favicon.ico', to: '.' },
+        { from: './src/favicon/favicon-16x16.png', to: '.' },
+        { from: './src/favicon/favicon-32x32.png', to: '.' },
+        { from: './src/favicon/apple-touch-icon.png', to: '.' },
+        { from: './src/favicon/android-chrome-192x192.png', to: '.' },
+        { from: './src/favicon/android-chrome-512x512.png', to: '.' },
+        { from: './src/favicon/site.webmanifest', to: '.' },
+      ]
+    })
   ],
 
   module: {
